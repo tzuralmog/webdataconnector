48a070f5f4d68944f9de6bfe8d3411d40c034911
tzur <Tzur.almog@gmail.com>
2021-11-01 10:08:03 -0400

    Started work on thing

---
+++ Examples/Kontakt.io/config.js
@@ -0,0 +1,10 @@
+// The necessary configuration for your server
+// Contains credentials for your Foursquare application
+// And the new redirect path for the OAuth flow
+module.exports = {
+ 'HOSTPATH': 'http://localhost',
+ 'PORT': 3333,
+ 'CLIENT_ID': 'OWMVRPSNUVEUDVLXJUUJEO0AWKVF3ELQPVWVSAMV5IDWZXC3',
+ 'CLIENT_SECRET': 'WAFO1HNW4AMEBFFY3XX2HDN1UW1ID5IZ0PNLJKVNWKKQYWCL',
+ 'REDIRECT_PATH': '/redirect'
+};
